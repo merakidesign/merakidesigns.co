36000294894cbdc636ce8e27c658888af8b3a0b9
Renata-Adrienn <bajkorenata@gmail.com>
2020-11-13 18:38:22 +0200

    added content

--- web/src/components/hero.js
+++ web/src/components/hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
       <div className={styles.wrapper}>
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0}}>
           <h2 className={styles.h6}>An Experience Design Studio</h2>
-          <h3>We design powerful strategy, customer experience and flexible design systems for contemporary brands and platforms.</h3>
+          <h3>We build intuitive websites and flexible design systems for lifestyle and wellness brands.</h3>
         </motion.div>
       </div>
     </section>
