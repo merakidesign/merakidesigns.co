11c5a3add3d19195228cc29ee4ac143eed9e080a
Septimiu Bloj <septimiubloj@gmail.com>
2020-11-27 20:08:52 +0200

    add Google Analytics

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -20,6 +20,17 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: process.env.GA_TRACKING_ID,
+        head: true,
+        anonymize: true,
+        respectDNT: true,
+        pageTransitionDelay: 0,
+      },
+    },
+    {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         custom: {
